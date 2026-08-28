5601c4c714e62387203c710a359a156aacde4d54
Altun Hasanli <hasanlialtun@gmail.com>
2026-08-28 11:44:53 +0300

    test(semantic): require canonical advisory parameter identity

---
+++ phalcom-semantic/tests/canonical_parameter_advisory.rs
@@ -0,0 +1,28 @@
+use std::collections::BTreeMap;
+
+use phalcom_common::selector::{Selector, SelectorSlot};
+use phalcom_semantic::{
+    AdvisoryConfidence, AdvisoryContributionSource, AdvisoryFact, AdvisoryParameterContributions, CallableId,
+    CallableParameterId, DeclarationId, DispatchSide, ModuleId, ValueShape,
+};
+
+#[test]
+fn advisory_parameter_contributions_use_canonical_parameter_identity() {
+    let callable = CallableId::new(
+        DeclarationId::new(ModuleId::core(), "Probe".into()),
+        Selector::method("consume", [SelectorSlot::Positional]).unwrap(),
+        DispatchSide::Instance,
+    );
+    let parameter = CallableParameterId::new(callable.clone(), 0);
+    let fact = AdvisoryFact::new(ValueShape::Unknown, AdvisoryConfidence::Interprocedural);
+    let mut contributions = AdvisoryParameterContributions::default();
+
+    let deltas = contributions.replace_source(
+        AdvisoryContributionSource::Callable(callable),
+        BTreeMap::from([(parameter.clone(), fact.clone())]),
+    );
+
+    assert_eq!(contributions.get(&parameter), Some(&fact));
+    assert_eq!(deltas.len(), 1);
+    assert_eq!(deltas[0].slot, parameter);
+}
