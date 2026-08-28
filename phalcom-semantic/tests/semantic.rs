88a324c9cc0a484a5f3b3e8dd08ca6d66dcf93a5
Altun Hasanli <hasanlialtun@gmail.com>
2026-08-28 08:45:05 +0300

    test(semantic): rename import provenance regression module

--- phalcom-semantic/tests/semantic.rs
+++ phalcom-semantic/tests/semantic.rs
@@ -6,5 +6,5 @@ mod semantic;
 #[path = "constructor_factory_probe.rs"]
 mod constructor_factory_probe;
 
-#[path = "module_query_probe.rs"]
-mod module_query_probe;
+#[path = "module_query_provenance.rs"]
+mod module_query_provenance;
